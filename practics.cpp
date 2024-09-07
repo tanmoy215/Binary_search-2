9f95bec7c08abbeb9f3aa205dac06f6e2891e6ac
Tanmoy <majitanmoy14@gmail.com>
2024-09-07 07:37:06 +0530

    sep 7

--- practics.cpp
+++ practics.cpp
@@ -1,6 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    
+    vector<int> nums{1,2,3,4,3,2,1};
+     int lo = 0;
+     int hi = nums.size()-1;
+     while(lo<=hi){
+        int mid = lo + (hi-lo)/2;
+        if(nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1]){
+            cout<<nums[mid]<<endl;
+            break;
+        }
+        else if(nums[mid]<nums[mid+1] && nums[mid]>nums[mid-1]) lo =mid+1;
+        else hi = mid-1;
+     }
     return 0;
 }
